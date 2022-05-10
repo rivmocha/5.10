2356e016a33358d2cf7534dc8ccf2e38748f045e
mocha <skykww0714@gmail.com>
2022-05-10 13:30:20 +0900

    x회만큼 입력한 수 합 함수

--- main.cpp
+++ main.cpp
@@ -4,17 +4,35 @@ using namespace std;
 
 
 
-int main()
+int ArraySum(int* Array, int count)		// 횟수만큼 받은 수 합을 함수로.
 {
-	int i = 0;
-	int count = 10;
-	int* Array = new int[count];
+	int Sum = 0;
 
-	for (int i = 0; i > count; ++i);
+	for (int i = 0; i < count; ++i)
 	{
-		Array[i] = i + 1;
+		Sum += Array[i];
 	}
 
+	return Sum;
+
+}
+
+int main()
+{
+	int count = 0;
+
+	cin >> "입력횟수" >> "  " >> count;		//횟수 입력
+
+	int* Array = new int[count];		//받은 횟수만큼 저장
+
+	for (int i = 0; i < count; ++i)		//횟수마다 배열 한칸씩 저장
+	{
+		cin >> Array[i];
+	}
+
+	int Sum = ArraySum(Array, count);
+
+	cout << Sum << endl;
 
 	return 0;
 }
