39ceaa03e2fc9b6a101a32f144935de3c5bb7cf7
mocha <skykww0714@gmail.com>
2022-05-10 10:28:47 +0900

    10회만큼 배열에 정수 받고 합 출력

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int i = 0;
+	int count = 10;
+	int* Array = new int[count];
+
+	for (int i = 0; i > count; ++i);
+	{
+		Array[i] = i + 1;
+	}
+
+
+	return 0;
+}
